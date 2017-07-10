c1a758b9cbd92fe110847a8c8858be191eda50b1
Litlpip307 <buharov.rd@net.ugatu.su>
2017-07-10 16:42:47 +0500

    Aga3

---
+++ dbo.Department.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Department] (
+    [Id]                INT            IDENTITY (1, 1) NOT NULL,
+    [Title]             NVARCHAR (MAX) NOT NULL,
+    [City]              NCHAR (50)     NOT NULL,
+    [Addess]            NCHAR (50)     NOT NULL,
+    [NumberOfEmployees] SMALLINT       NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
---
+++ dbo.TypesOfRequest.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[TypesOfRequest] (
+    [Id]    INT           IDENTITY (1, 1) NOT NULL,
+    [Title] NVARCHAR (50) NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
